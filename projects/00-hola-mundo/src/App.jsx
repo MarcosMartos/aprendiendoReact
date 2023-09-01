b09f4fcfb2d5973a52257227b4879f37d57154d3
Marcos Martos <marcosmartostuvi@gmail.com>
2023-09-01 12:00:11 -0300

    crear primeros elementos

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,20 @@
+export function App() {
+  return (
+    <article style={{ color: "#fff" }}>
+      <header>
+        <img
+          src="https://unavatar.io/kikobeats?ttl=1h"
+          alt="Avatar de un tipo"
+        />
+        <div>
+          <strong>Marcos Martos</strong>
+          <span>@MarcosMartos</span>
+        </div>
+      </header>
+
+      <aside>
+        <button>Seguir</button>
+      </aside>
+    </article>
+  );
+}
