01f82412757e9c1c9bf95883dcb473457cb146a4
Marcos Martos <marcosmartostuvi@gmail.com>
2023-09-01 12:27:10 -0300

    crear componente reutilizable con props

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,22 +1,11 @@
 import "./App.css";
+import { TwitterFollowCard } from "./TwitterFollowCard";
 
 export function App() {
   return (
-    <article style={{ color: "#fff" }}>
-      <header>
-        <img
-          src="https://unavatar.io/kikobeats?ttl=1h"
-          alt="Avatar de un tipo"
-        />
-        <div>
-          <strong>Marcos Martos</strong>
-          <span>@MarcosMartos</span>
-        </div>
-      </header>
-
-      <aside>
-        <button>Seguir</button>
-      </aside>
-    </article>
+    <>
+      <TwitterFollowCard userName={"MarcosMartos"} name={"Marcos Martos"} />
+      <TwitterFollowCard userName={"midudev"} name={"Miguel Ángel Durán"} />
+    </>
   );
 }
