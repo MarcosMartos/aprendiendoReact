c59be6c1c1fea33a4c10a019e0c5bb27235985f3
Marcos Martos <marcosmartostuvi@gmail.com>
2023-09-04 11:40:22 -0300

    mapear lista de usuarios

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -2,14 +2,43 @@ import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 
 export function App() {
+  const users = [
+    {
+      userName: "MarcosMartos",
+      name: "Marcos Martos",
+      isFollowing: false,
+    },
+    {
+      userName: "midudev",
+      name: " Miguel Ángel Durán",
+      isFollowing: false,
+    },
+    {
+      userName: "EnzoxxB",
+      name: "Enzo Balderrama",
+      isFollowing: false,
+    },
+    {
+      userName: "MatiasIkaczijk",
+      name: "Matías Ikaczijk",
+      isFollowing: false,
+    },
+  ];
+
   return (
     <section className="App">
-      <TwitterFollowCard initialIsFollowing={false} userName={"MarcosMartos"}>
-        Marcos Martos
-      </TwitterFollowCard>
-      <TwitterFollowCard initialIsFollowing={false} userName={"midudev"}>
-        Miguel Ángel Durán
-      </TwitterFollowCard>
+      {users.map((user) => {
+        const { userName, name, isFollowing } = user;
+        return (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        );
+      })}
     </section>
   );
 }
