1c95afb4f8e2ecc98f2a014cf38aff406e22cd17
Marcos Martos <marcosmartostuvi@gmail.com>
2023-09-04 11:28:01 -0300

    dar estilos a los botones

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -4,10 +4,10 @@ import { TwitterFollowCard } from "./TwitterFollowCard";
 export function App() {
   return (
     <section className="App">
-      <TwitterFollowCard isFollowing={true} userName={"MarcosMartos"}>
+      <TwitterFollowCard initialIsFollowing={false} userName={"MarcosMartos"}>
         Marcos Martos
       </TwitterFollowCard>
-      <TwitterFollowCard isFollowing={false} userName={"midudev"}>
+      <TwitterFollowCard initialIsFollowing={false} userName={"midudev"}>
         Miguel Ángel Durán
       </TwitterFollowCard>
     </section>
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,4 +1,17 @@
-export function TwitterFollowCard({ children, userName, isFollowing }) {
+import { useState } from "react";
+
+export function TwitterFollowCard({ children, userName, initialIsFollowing }) {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+
+  const text = isFollowing ? "Siguiendo" : "Seguir";
+  const buttonClassName = isFollowing
+    ? "tw-followCard-button is-following"
+    : "tw-followCard-button";
+
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  };
+
   return (
     <article className="tw-followCard">
       <header className="tw-followCard-header">
@@ -14,7 +27,10 @@ export function TwitterFollowCard({ children, userName, isFollowing }) {
       </header>
 
       <aside>
-        <button className="tw-followCard-button">Seguir</button>
+        <button className={buttonClassName} onClick={handleClick}>
+          {text}
+          <span className="tw-followCard-stopFollow">Dejar de Seguir</span>
+        </button>
       </aside>
     </article>
   );
