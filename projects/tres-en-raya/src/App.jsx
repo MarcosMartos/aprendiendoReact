9a721b8e16ee115e6b1b66ef4756166a659e4f15
Marcos Martos <marcosmartostuvi@gmail.com>
2023-09-05 11:18:22 -0300

    crear estado turn con hook useState

--- projects/tres-en-raya/src/App.jsx
+++ projects/tres-en-raya/src/App.jsx
@@ -6,12 +6,15 @@ const TURNS = {
 import { useState } from "react";
 import "./App.css";
 
-const Square = ({ children, updateBoard, index }) => {
-  return <div className="square">{children}</div>;
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+  return <div className={className}>{children}</div>;
 };
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
+
+  const [turn, setTurn] = useState(TURNS.X);
 
   return (
     <main className="board">
@@ -20,6 +23,11 @@ function App() {
         {board.map((_, index) => {
           return <Square key={index} index={index}></Square>;
         })}
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
   );
